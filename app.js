ef8cea05d120106f973389c847c50737a00f4df2
Sulaiman001 <shinaadeleye001@gmail.com>
2018-03-31 14:19:59 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,52 +1,72 @@
+/*-----------------------------------------------------------------------------
+A simple echo bot for the Microsoft Bot Framework. 
+-----------------------------------------------------------------------------*/
+
 var restify = require('restify');
 var builder = require('botbuilder');
+var botbuilder_azure = require("botbuilder-azure");
 
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
-
+  
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
     appId: process.env.MicrosoftAppId,
-    appPassword: process.env.MicrosoftAppPassword
+    appPassword: process.env.MicrosoftAppPassword,
+    openIdMetadata: process.env.BotOpenIdMetadata
 });
 
 // Listen for messages from users 
 server.post('/api/messages', connector.listen());
 
-// This bot enables users to either make a dinner reservation or order dinner.
-var inMemoryStorage = new builder.MemoryBotStorage();
+/*----------------------------------------------------------------------------------------
+* Bot Storage: This is a great spot to register the private state storage for your bot. 
+* We provide adapters for Azure Table, CosmosDb, SQL Azure, or you can implement your own!
+* For samples and documentation, see: https://github.com/Microsoft/BotBuilder-Azure
+* ---------------------------------------------------------------------------------------- */
 
-// Main menu
+var tableName = 'botdata';
+var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.env['AzureWebJobsStorage']);
+var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
+
+// Create your bot with a function to receive messages from the user
+var bot = new builder.UniversalBot(connector);
+bot.set('storage', tableStorage);
+
+bot.on('conversationUpdate', function(message) {
+    // Send a hello message when bot is added
+    if (message.membersAdded) {
+        message.membersAdded.forEach(function(identity) {
+            if (identity.id === message.address.bot.id) {
+                var reply = new builder.Message().address(message.address).text("Hi! Welcome to LaundryMann.");
+                bot.send(reply);
+            }
+        });
+    }
+});
+
 var menuItems = { 
-    "Order dinner": {
-        item: "orderDinner"
+    "CARE UNLIMITED LAUNDRY PACK ₦35,000": {
+        item: "careUnlimitedPack"
     },
-    "Dinner reservation": {
-        item: "dinnerReservation"
+    "CARE MAKE SENSE PACK ₦30,000": {
+        item: "careMakesensePack"
     },
-    "Schedule shuttle": {
-        item: "scheduleShuttle"
+    "CARE NOT SINGLE PACK ₦15,450": {
+        item: "careNotsinglePack"
     },
-    "Request wake-up call": {
-        item: "wakeupCall"
+    "CARE SINGLE PACK ₦12,500": {
+        item: "careSinglePack"
     },
 }
 
-// This is a reservation bot that has a menu of offerings.
-var bot = new builder.UniversalBot(connector, [
-    function(session){
-        session.send("Welcome to Contoso Hotel and Resort.");
-        session.beginDialog("mainMenu");
-    }
-]).set('storage', inMemoryStorage); // Register in-memory storage 
 
-// Display the main menu and start a new request depending on user input.
 bot.dialog("mainMenu", [
     function(session){
-        builder.Prompts.choice(session, "Main Menu:", menuItems);
+        builder.Prompts.choice(session, "Our Packages:", menuItems);
     },
     function(session, results){
         if(results.response){
@@ -57,284 +77,32 @@ bot.dialog("mainMenu", [
 .triggerAction({
     // The user can request this at any time.
     // Once triggered, it clears the stack and prompts the main menu again.
-    matches: /^main menu$/i,
+    matches: /^your package$/i,
     confirmPrompt: "This will cancel your request. Are you sure?"
 });
 
-// This dialog prompts the user for a phone number. 
-// It will re-prompt the user if the input does not match a pattern for phone number.
-bot.dialog('phonePrompt', [
-    function (session, args) {
-        if (args && args.reprompt) {
-            builder.Prompts.text(session, "Enter the number using a format of either: '(555) 123-4567' or '555-123-4567' or '5551234567'")
-        } else {
-            builder.Prompts.text(session, "What's your phone number?");
-        }
+bot.dialog('greetings',[ 
+    function (session) {
+        session.beginDialog('askName');
     },
     function (session, results) {
-        var matched = results.response.match(/\d+/g);
-        var number = matched ? matched.join('') : '';
-        if (number.length == 10 || number.length == 11) {
-            session.userData.phoneNumber = number; // Save the number.
-            session.endDialogWithResult({ response: number });
-        } else {
-            // Repeat the dialog
-            session.replaceDialog('phonePrompt', { reprompt: true });
-        }
+        session.endDialog(`Hello ${results.response}, My name is Eve. Checkout LaundryMann Packages below!`);
+    }
+]);
+bot.dialog('askName', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.endDialogWithResult(results);
+		session.beginDialog("mainMenu");
     }
 ]);
 
 
-// This dialog helps the user make a dinner reservation.
-bot.dialog('dinnerReservation', [
-    function (session) {
-        session.send("Welcome to the dinner reservation.");
-        session.beginDialog('askForDateTime');
-    },
-    function (session, results) {
-        session.dialogData.reservationDate = builder.EntityRecognizer.resolveTime([results.response]);
-        session.beginDialog('askForPartySize');
-    },
-    function (session, results) {
-        session.dialogData.partySize = results.response;
-        session.beginDialog('askForReserverName');
-    },
-    function (session, results) {
-        session.dialogData.reservationName = results.response;
+/*
 
-        // Process request and display reservation details
-        session.send(`Reservation confirmed. Reservation details: <br/>Date/Time: ${session.dialogData.reservationDate} <br/>Party size: ${session.dialogData.partySize} <br/>Reservation name: ${session.dialogData.reservationName}`);
-        session.endDialog();
-    }
-])
-.endConversationAction(
-    "endOrderDinner", "Ok. Goodbye.",
-    {
-        matches: /^cancel$|^goodbye$|^end$|^stop$/i,
-        confirmPrompt: "This will cancel your reservation. Are you sure?"
-    })
-.triggerAction({
-    matches: /^dinner reservation$/i,
-    confirmPrompt: "This will cancel your current request. Are you sure?"
+bot.dialog('/', function (session) {
+    session.send('You said ' + session.message.text);
 });
-
-
-
-// This dialog help the user order dinner to be delivered to their hotel room.
-var dinnerMenu = {
-    "Potato Salad - $5.99": {
-        Description: "Potato Salad",
-        Price: 5.99
-    },
-    "Tuna Sandwich - $6.89": {
-        Description: "Tuna Sandwich",
-        Price: 6.89
-    },
-    "Clam Chowder - $4.50":{
-        Description: "Clam Chowder",
-        Price: 4.50
-	},	
-    "Check out": {
-		Description: "Check out",
-        Price: 0 // Order total. Updated as items are added to order.
-    },
-	    "Cancel order": { // Cancel the order and back to Main Menu
-        Description: "Cancel order",
-        Price: 0
-    }
-};
-
-
-// Add dinner items to the list by repeating this dialog until the user says `check out`. 
-bot.dialog("addDinnerItem", [
-    function(session, args){
-        if(args && args.reprompt){
-            session.send("What else would you like to have for dinner tonight?");
-        }
-        else{
-            // New order
-            // Using the conversationData to store the orders
-            session.conversationData.orders = new Array();
-            session.conversationData.orders.push({ 
-                Description: "Check out",
-                Price: 0
-            })
-        }
-        builder.Prompts.choice(session, "Dinner menu:", dinnerMenu);
-    },
-    function(session, results){
-        if(results.response){
-            if(results.response.entity.match(/^check out$/i)){
-                session.cancelDialog(0, "mainMenu");			
-            }
-			else if(results.response.entity.match(/^cancel/i)){
-                // Cancel the order and start "mainMenu" dialog.
-                session.cancelDialog(0, "mainMenu");
-            }
-            else {
-                var order = dinnerMenu[results.response.entity];
-                session.conversationData.orders[0].Price += order.Price; // Add to total.
-                var msg = `You ordered: ${order.Description} for a total of $${order.Price}.`;
-                session.send(msg);
-                session.conversationData.orders.push(order);
-                session.replaceDialog("addDinnerItem", { reprompt: true }); // Repeat dinner menu
-            }
-        }
-    }
-])
-.reloadAction(
-    "restartOrderDinner", "Ok. Let's start over.",
-    {
-        matches: /^start over$/i
-    }
-);
-
-
-// Menu: "Order dinner"
-// This dialog allows user to order dinner and have it delivered to their room.
-bot.dialog('orderDinner', [
-    function(session){
-        session.send("Lets order some dinner!");
-        session.beginDialog("addDinnerItem");
-    },
-    function (session, results) {
-        if (results.response) {
-            // Display itemize order with price total.
-            for(var i = 1; i < session.conversationData.orders.length; i++){
-                session.send(`You ordered: ${session.conversationData.orders[i].Description} for a total of $${session.conversationData.orders[i].Price}.`);
-            }
-            session.send(`Your total is: $${session.conversationData.orders[0].Price}`);
-
-            // Continue with the check out process.
-            builder.Prompts.text(session, "What is your room number?");
-        } 
-    },
-    function(session, results){
-        if(results.response){
-            session.dialogData.room = results.response;
-            var msg = `Thank you. Your order will be delivered to room #${results.response}`;
-            session.send(msg);
-            session.replaceDialog("mainMenu");
-        }
-    }
-])
-.reloadAction('startOver', 'Ok, starting over.', {
-    matches: /^start over$/i,
-    dialogArgs: {
-        isReloaded: true
-    }
-})
-
-.triggerAction({
-    matches: /^order dinner$/i
-})
-.reloadAction(
-    "restartOrderDinner", "Ok. Let's start over.",
-    {
-        matches: /^start over$|^New order$/i
-    }
-)
-.cancelAction(
-    "cancelOrder", "Type 'Main Menu' to continue.", 
-    {
-        matches: /^cancel$|^nevermind$|^cancel.*order/i,
-        confirmPrompt: "This will cancel your current order. Are you sure?"
-    }
-)
-
-// Once triggered, will start the 'showDinnerCart' dialog.
-// Then, the waterfall will resumed from the step that was interrupted.
-.beginDialogAction('showCartAction', 'showDinnerCart', {
-    matches: /^show cart$/i,
-    dialogArgs: {
-        showTotal: true
-    }
-})
-
-.endConversationAction('endConversationAction', 'Ok, goodbye!', {
-    matches: /^goodbye$|^bye$|^end$/i
-});
-
-
-// Show dinner items in cart with the option to show total or not.
-bot.dialog('showDinnerCart', function(session, args){
-    for(var i = 1; i < session.conversationData.orders.length; i++){
-        session.send(`You ordered: ${session.conversationData.orders[i].Description} for a total of $${session.conversationData.orders[i].Price}.`);
-    }
-
-    if(args && args.showTotal){
-        // End this dialog with total.
-        session.endDialog(`Your total is: $${session.conversationData.orders[0].Price}`);
-    }
-    else{
-        session.endDialog(); // Ends without a message.
-    }
-});
-
-
-
-// Dialog to ask for a date and time
-bot.dialog('askForDateTime', [
-    function (session) {
-        builder.Prompts.time(session, "Please provide a reservation date and time (e.g.: June 6th at 5pm)");
-    },
-    function (session, results) {
-        session.endDialogWithResult(results);
-    }
-]);
-
-// Dialog to ask for number of people in the party
-bot.dialog('askForPartySize', [
-    function (session) {
-        builder.Prompts.text(session, "How many people are in your party?");
-    },
-    function (session, results) {
-        session.endDialogWithResult(results);
-    }
-])
-//Getting Help for Restaurant Accommodating Capabilities
-	.beginDialogAction('partySizeHelpAction', 'partySizeHelp', { matches: /^help$/i });
-	// Context Help dialog for party size
-	bot.dialog('partySizeHelp', function(session, args, next) {
-    var msg = "Party size help: Our restaurant can support party sizes up to 150 members.";
-    session.endDialog(msg);
-	})
-
-// Dialog to ask for the reservation name.
-bot.dialog('askForReserverName', [
-    function (session) {
-        builder.Prompts.text(session, "Who's name will this reservation be under?");
-    },
-    function (session, results) {
-        session.endDialogWithResult(results);
-    }
-]);
-
-
-
-
-// The dialog stack is cleared and this dialog is invoked when the user enters 'help'.
-
-bot.dialog('help', function (session, args, next) {
-    session.endDialog("This is a bot that can help you make a dinner reservation. <br/>Please say 'next' to continue");
-})
-.triggerAction({
-    matches: /^help$/i,
-	    onSelectAction: (session, args, next) => {
-        // Add the help dialog to the top of the dialog stack 
-        // (override the default behavior of replacing the stack)
-        session.beginDialog(args.action, args);
-}});
-/***
-bot.dialog('help', function (session, args, next) {
-    session.endDialog("This is a bot that can help you make a dinner reservation. <br/>Please say 'next' to continue");
-})
-.triggerAction({
-    matches: /^help$/i,
-    onSelectAction: (session, args, next) => {
-        // Add the help dialog to the dialog stack 
-        // (override the default behavior of replacing the stack)
-        session.beginDialog(args.action, args);
-    }
-});
-**/
+*/
